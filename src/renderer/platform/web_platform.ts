4e3a3b92f38f96eab9d1c8f0e5bb647c08afd1bc
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-12-28 21:47:03 +0800

    remove: Disable Google Analytics tracking functionality
    Disable Google Analytics tracking in initTracking and trackingEvent methods.

--- src/renderer/platform/web_platform.ts
+++ src/renderer/platform/web_platform.ts
@@ -94,27 +94,12 @@ export default class WebPlatform extends IndexedDBStorage implements Platform {
   }
 
   public async initTracking() {
-    const GAID = 'G-B365F44W6E'
-    try {
-      const conf = await this.getConfig()
-      window.gtag('config', GAID, {
-        app_name: 'chatbox',
-        user_id: conf.uuid,
-        client_id: conf.uuid,
-        app_version: await this.getVersion(),
-        chatbox_platform_type: 'web',
-        chatbox_platform: await this.getPlatform(),
-        app_platform: await this.getPlatform(),
-      })
-    } catch (e) {
-      window.gtag('config', GAID, {
-        app_name: 'chatbox',
-      })
-      throw e
-    }
+    // Google Analytics disabled
+    return
   }
   public trackingEvent(name: string, params: { [key: string]: string }) {
-    window.gtag('event', name, params)
+    // Google Analytics disabled
+    return
   }
 
   public async shouldShowAboutDialogWhenStartUp(): Promise<boolean> {
